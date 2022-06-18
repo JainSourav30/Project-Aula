d436f08b8eb3253004159b9fd10d4a9a1d86170c
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-06-18 17:40:56 +0530

    Add Signup-one

---
+++ Raghav/Frontend/loginpage/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({value}) {
+  return (
+    <div>
+        <button className="btn">{value}</button>
+    </div>
+  )
+}
+
+export default Button
--- Raghav/Frontend/loginpage/src/components/LoginForm.js
+++ Raghav/Frontend/loginpage/src/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import './login.css'
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+
 function LoginForm({ Login, error }) {
 
     const [details, setDetails] = useState({name: "", email: "", password: ""});
@@ -11,10 +13,11 @@ function LoginForm({ Login, error }) {
     }
 
   return (
+    //<Router>
     <form onSubmit = {submitForm}>
         <div className="form-inner">
             <h2><span className = "head">AULA</span></h2>
-            {(error != "") ? (<div className="error">{error}</div>) : ""}
+            {(error !== "") ? (<div className="error">{error}</div>) : ""}
             <div className="form-elements">
                 <div className="form-group">
                     <label htmlFor = "name">Name :</label>
@@ -28,10 +31,16 @@ function LoginForm({ Login, error }) {
                 <label htmlFor = "password">Password :</label>
                     <input type="password" name="password" id="password" onChange={e => setDetails({...details, password : e.target.value })} value = {details.password}/>
                 </div>
-                <input type="submit" value = "LOGIN" />
+                <input type="submit" value = "Log-In" />
+                {/* <Router>
+            <Link to="./Signup-one.js">
+              <button>HELLO</button>
+            </Link>
+          </Router> */}
             </div>
         </div>
     </form>
+    //</Router>
   )
 }
 
